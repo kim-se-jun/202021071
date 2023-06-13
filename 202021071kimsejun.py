6ff444467fdf44554ae5a8634c351dc73c565906
kim-se-jun <136061185+kim-se-jun@users.noreply.github.com>
2023-06-13 13:45:40 +0900

    Update 202021071kimsejun.py

--- 202021071kimsejun.py
+++ 202021071kimsejun.py
@@ -3,12 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import control
 
-def main():
-  st.write('202021071 김세준')
-  
-if__name__=='__main__':
-  main()
-
 # 전달함수 G(s) 정의
 num = [100] # 분자 계수
 den = [1, 5, 6] # 분모 계수: s^2 + 5s + 6
